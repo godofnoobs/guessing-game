505b1623b2615349d2cb4fa86254d5f081af8b3d
godofnoobs <n_ska@bk.ru>
2018-02-20 15:13:47 +0300

    class realized

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(name) {
+        this.name = name;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
-
+        var sum = this.max+this.min;
+        this.current = (sum%2) ? (sum+1)/2 : sum/2;
+        return this.current;
     }
 
     lower() {
-
+        this.setRange(this.min, this.current);
     }
 
     greater() {
-
+        this.setRange(this.current, this.max);
     }
 }
 
